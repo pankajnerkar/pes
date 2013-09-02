d7a2e97cba35b8d6f6f085d3931993e3d082a16d
Prabhas Gupte <prabhas.gupte@gmail.com>
2013-09-02 15:23:11 +0530

    It is observed that every third number is fibonacci series is even. So, directly adding every third number to $sum, and got rid of if condition.

--- pes2.php
+++ pes2.php
@@ -1,15 +1,17 @@
 <?php
+/*
+ * It is observed that every third number is fibonacci series is even.
+ * So, directly adding every third number to $sum, and got rid of if condition.
+ */
 $n1 = 1;
-$n2 = 2;
-$sum = 2;
-$i = 2;
-while($i < 4000000) {
-        $i = $n1 + $n2;
-        $n1 = $n2;
-        $n2 = $i;
-        if ($i%2 == 0) {
-                $sum += $i;
-        }
+$n2 = 1;
+$n3 = $n1 + $n2;
+$sum = 0;
+while($n3 < 4000000) {
+	$sum += $n3;
+	$n1 = $n2 + $n3;
+	$n2 = $n1 + $n3;
+	$n3 = $n1 + $n2;
 }
 echo $sum;
 ?>
