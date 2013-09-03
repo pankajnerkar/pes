829ce631e2f1de0571e8b4065a0e59034f566999
Prabhas Gupte <prabhas.gupte@gmail.com>
2013-09-03 13:06:36 +0530

    Issue 1 fixed. Added problem statements to all 5 files that are present in repo.

--- pes3.php
+++ pes3.php
@@ -1,4 +1,10 @@
 <?php
+/**
+ * Problem 3: Largest prime factor
+ *
+ * The prime factors of 13195 are 5, 7, 13 and 29.
+ * What is the largest prime factor of the number 600851475143 ?
+ */
 $n = 600851475143;
 $factor = 2;
 $lastFactor = 1;
--- pes4.php
+++ pes4.php
@@ -1,4 +1,10 @@
 <?php
+/**
+ * Problem 4: Largest palindrome product
+ *
+ * A palindromic number reads the same both ways. The largest palindrome made from the product of two 2-digit numbers is 9009 = 91 × 99.
+ * Find the largest palindrome made from the product of two 3-digit numbers.
+ */
 $largest=101;
 $l1 = $l2 = 999;
 for ($i=999; $i>=100; $i--) {
